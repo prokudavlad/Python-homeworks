fd2b6e2b27bd232e0e518a60bcef529685de1fb7
prokudavlad <vladserg92@gmail.com>
2022-12-13 14:02:20 +0200

    homework commit

--- hw-python1.py
+++ hw-python1.py
@@ -1,7 +1,34 @@
-a = int(input("Enter an integer number A: "))
+# Создать python модуль для реализации ввода через клавиатуру целочисленних значений
+# a = int(input("Enter an integer number A: "))
 
-b = int(input("Enter an integer number B: "))
+# b = int(input("Enter an integer number B: "))
 
-s = a + b
+# s = a + b
 
-print("Sum of numbers is: ", s)
+# print("Sum of numbers is: ", s)
+
+# Добавить функционал конвертации во float и str. Вивести результат на печать
+
+int(390.8)
+b = 125.0
+c = 390.8
+print(int(b))
+print(int(c))
+f = 57
+print(float(f))
+b = 125.0
+c = 390.8
+print(int(b))
+print(int(c))
+print(str(421.134))
+f = 5555.55
+print(str(f))
+f = 5555.55
+print("Vladislav has " + str(f) + " points.")
+user = "Vladislav"
+lines = 55
+print("Congratulations, " + user + "! You just wrote " + str(lines) + " lines of code.")
+total_points = "5524.53"
+new_points = "45.30"
+new_total_points = total_points + new_points
+print(new_total_points)
