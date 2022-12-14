75a7a7d4047f3476779e86a4071f3afedcc44251
prokudavlad <vladserg92@gmail.com>
2022-12-14 15:19:12 +0200

    homework6 commit

---
+++ hw-python6.py
@@ -0,0 +1,11 @@
+num1 = 99
+num2 = 77
+num3 = 23
+
+if (num1 >= num2) and (num1 >= num3):
+    largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+    largest = num2
+else:
+    largest = num3
+print("The largest number is", largest)
