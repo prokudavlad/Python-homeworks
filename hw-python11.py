f477eb7544fd45f1d31b2dd9947ef5954b824ab6
prokudavlad <vladserg92@gmail.com>
2022-12-21 17:36:02 +0200

    homework11 commit

---
+++ hw-python11.py
@@ -0,0 +1,87 @@
+# my_string = "Python"
+# for mth in dir(my_string):
+#    print(mth)
+
+# my_string = my_string.upper()
+# print(my_string)
+# my_string = my_string.lower()
+# print(my_string)
+# my_string = my_string.title()
+# print(my_string)
+
+# my_string = "Formatting, String Methods, String Slices in Python"
+# print(my_string)
+# my_string = my_string.replace("Python", "Python3")
+# print(my_string)
+
+# my_string = "Splits the string at the specified separator, and returns a list"
+# my_string = my_string.split(" ")
+# print(my_string)
+
+# my_string = " Splits " + " the " + " string "
+# print(my_string)
+
+# my_string = "-".join(my_string)
+# print(my_string)
+
+# my_string = "Returns a trimmed version of the string!"
+# my_string = my_string.strip()
+# print(my_string)
+
+# my_sting = my_string.strip("!")
+# print(my_string)
+
+# my_string = "@ Returns a left trim version of the string !"
+# print(my_string)
+# my_string = my_string.lstrip("@")
+# print(my_string)
+# my_string = my_string.rstrip("!")
+# print(my_string)
+
+# name = "Python"
+# my_string = f"Hello {name}!"
+# print(my_string)
+
+# name_1 = "have a"
+# nam_2 = "nice day"
+# my_string = f"Hello {name}, {name_1} {nam_2}!"
+# print(my_string)
+
+# name = "StarLink"
+# my_string = "SpaceX {0}{1}".format(name, "!")
+# print(my_string)
+
+# name = "Vladislav"
+# my_string = "Hello %s"
+# print(my_string % name)
+
+# my_string = "Python is awesome"
+# print(my_string)
+# print(my_string[0])
+# print(my_string[1])
+# print(my_string[2])
+# print(my_string[3])
+# print(my_string[4])
+# print(my_string[5])
+# print(my_string[6])
+
+# print(len(my_string))
+# print(my_string[17])
+# print(my_string[len(my_string) - 1])
+# print(my_string[-1])
+# print(my_string[-2])
+# print(my_string[-3])
+# print(my_string[-4])
+# print(my_string[-5])
+# print(my_string[-6])
+
+# my_string = my_string[0:6]
+# print(my_string)
+# my_string = my_string[7:9]
+# print(my_string)
+# print(my_string[:])
+
+# my_string = "123456789"
+# print(my_string)
+# print(my_string[::2])
+# print(my_string[::-1])
